1f105bc55e5d6a13b7cd9470f748ade26a3a0204
ivanlai <ivan.ch.lai@gmail.com>
2020-10-03 17:29:17 +0100

    Add setup.sh and requirements.txt

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install git
+sudo apt install python3-pip
+
+sudo pip3 install gunicorn
+sudo pip3 install -r requirements.txt
+
+git clone https://github.com/ivanlai/apps-UK_houseprice.git
+
