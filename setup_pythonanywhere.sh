87e5ce8daa322a643c1ac51242e8d8a13bed4366
ivanlai <ivan.ch.lai@gmail.com>
2020-10-03 18:49:48 +0100

    x

--- setup_pythonanywhere.sh
+++ setup_pythonanywhere.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-pip3.6 install gunicorn --user
-pip3.6 install -r requirements.txt --user
+pip3 install -r requirements.txt --user
 
 # git clone https://github.com/ivanlai/apps-UK_houseprice.git
