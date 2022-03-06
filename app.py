9cad438328882207063495d8fbc744a9be00a3a3
ivanlai <ivan.ch.lai@gmail.com>
2022-03-06 21:27:23 +0000

    Dec 2021 update

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ cfg['start_year']       = 1995
 cfg['end_year']         = 2021
 
 cfg['Years']            = list(range(cfg['start_year'], cfg['end_year']+1))
-cfg['latest date']      = "31 October 2021"
+cfg['latest date']      = "31 December 2021"
 
 #When running in Pythonanywhere
 appDataPath = '/home/ivanlai/apps-UK_houseprice/appData'
